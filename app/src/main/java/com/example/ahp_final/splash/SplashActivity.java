42671a42218a4d5c556e4129aabea31d1b477650
Franco Gonzalo Pacori Aliaga <franco_pacori@hotmail.com>
2019-10-25 17:58:20 -0500

    Creacion de SplashScreen

---
+++ app/src/main/java/com/example/ahp_final/splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.ahp_final.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ahp_final.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
