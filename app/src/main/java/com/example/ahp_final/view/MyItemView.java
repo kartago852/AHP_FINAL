e17e25aed7edd8b8d81d008fe32c1018a1ede835
Franco Pacori Aliaga <franco_pacori@hotmail.com>
2019-10-27 00:35:27 -0500

    Configurando MyItemView

--- app/src/main/java/com/example/ahp_final/view/MyItemView.java
+++ app/src/main/java/com/example/ahp_final/view/MyItemView.java
@@ -1,4 +1,17 @@
 package com.example.ahp_final.view;
 
-public class MyItemView {
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.widget.LinearLayout;
+
+@SuppressLint("ViewConstructor")
+public class MyItemView extends LinearLayout {
+
+    private int position = 0;
+    private String hint = "";
+
+    public MyItemView(Context context, int position, String hint){
+        super(context);
+    }
+
 }
