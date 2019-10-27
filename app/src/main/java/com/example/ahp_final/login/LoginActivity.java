f66f6a6229105bbbb001ea12a9838256c8c8008c
Franco Pacori Aliaga <franco_pacori@hotmail.com>
2019-10-27 00:01:47 -0500

    Creando Login

---
+++ app/src/main/java/com/example/ahp_final/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.ahp_final.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ahp_final.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_login );
+    }
+}
