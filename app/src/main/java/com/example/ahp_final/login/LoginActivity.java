68521c40f783015f0d3014e40deabb9f14c249fb
Franco Gonzalo Pacori Aliaga <franco_pacori@hotmail.com>
2019-10-29 10:28:59 -0500

    Creando vista login

--- app/src/main/java/com/example/ahp_final/login/LoginActivity.java
+++ app/src/main/java/com/example/ahp_final/login/LoginActivity.java
@@ -1,16 +1,31 @@
 package com.example.ahp_final.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.ahp_final.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private long backPressedTime;
+    String email,pass;
+
+    private CardView Login,Register;
+    private FirebaseAuth firebaseAuth;
+    private EditText TextEmail;
+    private EditText TextPasss;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login );
+
+
     }
 }
