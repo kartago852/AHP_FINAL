89c7a4eec72223f57778a6f0cc022c3b5ca7615f
Franco Pacori Aliaga <franco_pacori@hotmail.com>
2019-11-01 02:33:23 -0500

    mostrando el nombre del usuario, corregiendo homeactivity y su layout

--- app/src/main/java/com/example/ahp_final/home/HomeActivity.java
+++ app/src/main/java/com/example/ahp_final/home/HomeActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.ahp_final.R;
 import com.example.ahp_final.input.Criterio;
@@ -15,12 +16,17 @@ public class HomeActivity extends AppCompatActivity {
     public static final String user = "names";
 
     Button startBtn;
+    TextView txtuser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_home );
+
+
+        txtuser = (TextView)findViewById( R.id.texser );
         String user = getIntent().getStringExtra("names");
+        txtuser.setText( "!Bienvenido "+ user + "!" );
 
         initStartButton();
     }
